6df03a4822f5fedc74340f0dbb707447ac5eb727
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-27 21:30:52 +0300

    Update IPv4.java

--- IPv4.java
+++ IPv4.java
@@ -3,8 +3,9 @@ package com.company;
 /**
  * Created by Student on 27.02.2018.
  */
+
 public class IPv4 {
-    int ipv4[];
+    private int ipv4[];
 
     IPv4(int ipv4[]) { // Если в конструктор передается готовый IP
         this.ipv4 = ipv4;
@@ -12,26 +13,36 @@ public class IPv4 {
 
     IPv4(String ipStr) { //Если в конструктор передается строка
         char charIn;
-        String tmpStr = "";
+
         int dotNow = 0;
         int octNow = 0;
+        int[] ipv4C = new int[4];
 
         for (int i = 0; i < ipStr.length(); i++) {
             charIn = ipStr.charAt(i);
             //if (octNow>3) throw new Exception("Число октетов больше 4!");
             //if (charIn > '0' & charIn < 9 & charIn != '.') throw new Exception("Неверный символ в IP адресе");
-            if (charIn == '.') {
-                for (int j = dotNow; j < i; j++) {
+            if(octNow<3) {
+                if (charIn == '.') {
+                    String tmpStr = "";
+                    for (int j = dotNow; j < i; j++) {
+                        tmpStr += ipStr.charAt(j);
+                    }
+                    dotNow = i + 1;
+                    ipv4C[octNow] = Integer.parseInt(tmpStr);
+                    octNow++;
+                }
+            }
+            else {
+                String tmpStr = "";
+                for (int j = dotNow; j < ipStr.length(); j++) {
                     tmpStr += ipStr.charAt(j);
                 }
-                dotNow = i + 1;
-                ipv4[octNow] = Byte.parseByte(tmpStr);
-                octNow++;
+                ipv4C[octNow] = Integer.parseInt(tmpStr);
+                break;
             }
-
         }
-
-
+        ipv4=ipv4C;
     }
 
     public String printDecBin() {
@@ -43,12 +54,12 @@ public class IPv4 {
     }
 
     public String printIPBin() {
-        String decStr = "";
+        String binStr = "";
         for (int i = 0; i < 4; i++) {
-            if (i < 3) decStr += intToEightDigBin(ipv4[i]) + ".";
-            else decStr += intToEightDigBin(ipv4[i]);
+            if (i < 3) binStr += intToEightDigBin(ipv4[i]) + ".";
+            else binStr += intToEightDigBin(ipv4[i]);
         }
-        return decStr;
+        return binStr;
     }
 
     public String printIPDec() {
@@ -70,6 +81,4 @@ public class IPv4 {
         octBinOut += octBinIn;
         return octBinOut;
     }
-
-
 }
