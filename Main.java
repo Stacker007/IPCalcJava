7686008baab54f2dee415e98c95be575814d8313
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-02-28 21:24:50 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,34 +1,11 @@
 package com.company;
+import java.util.*;
 
-import java.io.*;
+public class Main {
 
-public class Main{
+    public static void main(String[] args) {
 
-    public static void main(String[] args){
-
-        int[] one = {192,168,0,1};
-        int[] two = {192,168,200,111};
-
-        IPv4 ohe = new IPv4(one);
-        IPv4 tw = new IPv4(two);
-        IPv4 three = new IPv4("192.168.12.111");
-
-        System.out.println(ohe.printIPBin());
-        System.out.println(ohe.printIPDec());
-        System.out.println(ohe.printDecBin());
-        System.out.println(tw.printDecBin());
-
-        System.out.println(tw.printIPBin());
-        System.out.println(tw.printIPDec());
-        System.out.println(three.printDecBin());
-
-        System.out.println(three.printIPBin());
-        System.out.println(three.printIPDec());
-
-
-
-
-
+        PrintNet.letPrint();
 
 
     }
